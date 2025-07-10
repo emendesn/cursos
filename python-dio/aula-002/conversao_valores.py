6ed687b52f48010d67d9f28c2219de4de469ff03
Edilson Mendes Nascimento <edilson.mendes.nascimento>
2025-07-10 19:29:03 -0300

    Auala relacionada a conversao de valores

---
+++ python-dio/aula-002/conversao_valores.py
@@ -0,0 +1,23 @@
+preco = 10.5
+idade  = 28
+
+print((str(preco)))
+
+print(str(idade))
+
+texto = f'idade {idade} preco {preco}'
+print(texto)
+
+print(int(1.943))
+print(int('10'))
+print(float(10.10))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+print(100 // 2)
