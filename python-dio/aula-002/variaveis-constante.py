9c9bf0dfd77406bfb643445f163bbcb9c0c2eedb
Edilson Mendes Nascimento <edilson.mendes.nascimento>
2025-07-10 19:07:09 -0300

    Aula relacionando as variaveis e constantes

---
+++ python-dio/aula-002/variaveis-constante.py
@@ -0,0 +1,16 @@
+age = 23
+name = 'Guilherme'
+print( f'Menu nome e {name} e eu tenho {age} ano(s) de idade.')
+
+age, name = (23, 'Guilherme')
+print( f'Menu nome e {name} e eu tenho {age} ano(s) de idade.')
+
+nome = 'Guilhermina'
+
+idade = 31
+
+print( nome, idade )
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'MG']
+
+print(BRAZILIAN_STATES)
