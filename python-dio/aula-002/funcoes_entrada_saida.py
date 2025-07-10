269fafc18af401515ffa8783f1aa0af09ea95550
Edilson Mendes Nascimento <edilson.mendes.nascimento>
2025-07-10 19:57:20 -0300

    Funcoes para entrada e saida

---
+++ python-dio/aula-002/funcoes_entrada_saida.py
@@ -0,0 +1,7 @@
+nome = input('Informe o seu nome:')
+sobrenome = input('Informe o sobrenome:')
+
+print( nome, sobrenome)
+print( nome, sobrenome, end='..\n')
+print( nome, sobrenome, sep='#')
+print( f'O nome informado eh {nome} {sobrenome}' )
